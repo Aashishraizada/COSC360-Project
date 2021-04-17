1797f9f8b36672a992a521e4e542ba00cbbdd9c4
Nakheem <41401736+Nakheem@users.noreply.github.com>
2021-04-17 03:27:44 -0700

    Post
    post kinda works

--- scripts/newPost.php
+++ scripts/newPost.php
@@ -5,11 +5,12 @@
 
 if ($_SERVER['REQUEST_METHOD']=="POST"||$_SERVER['REQUEST_METHOD']=="post") {
     
-    if (isset($_POST['username']) && isset($_POST['content']) && isset($_POST['image'])) {
+    if (isset($_POST['username'])&& isset($_POST['title']) && isset($_POST['content']) && isset($_POST['image'])) {
         
         $username = $_POST['username'];
         $content = $_POST['content'];
-        $image = $_Post['image'];
+        $image = $_POST['image'];
+        $title = $_POST['title'];
         $likes = 0;
         
         $host = "sql3.freesqldatabase.com";
@@ -33,17 +34,16 @@ if ($_SERVER['REQUEST_METHOD']=="POST"||$_SERVER['REQUEST_METHOD']=="post") {
             $found = false;
             while ($row = mysqli_fetch_assoc($results)) {
                 if($row['username']==$username){
-                    echo "User already exists with this name and/or email <br/>";
-                    echo "<a href=\"http://localhost/lab9/lab9-1.html\">Return to user entry</a>";
                     $found = true;
                 }
             }
             //if user doesn't exist then insert into database
             if($found==true){
-                $sql = "INSERT INTO Post (content,username,date, likes,image) VALUES ('".$content."', '".$username."', 'GETDATE()', '".$likes."', '".$image."')";
+                $sql = "INSERT INTO Post (title,content,username,date, likes,image) VALUES ('".$title."','".$content."', '".$username."', 'GETDATE()', '".$likes."', '".$image."')";
                 if (mysqli_query($connection, $sql)){
                     echo " ".$username." has made a post";
-                    echo "<a href=\"http://localhost/lab9/lab9-1.html\">Return to user entry</a>";
+                    header("Location: http://localhost/COSC360-Project/html/index.html", TRUE, 301);
+                    exit();
                 }else{
                     echo "Error: " . $sql . "<br>" . mysqli_error($connection);
                 }

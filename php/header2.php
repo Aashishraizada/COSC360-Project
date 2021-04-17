6f4d43bd4a9e52b132e4860013e1b56f319c578d
Nakheem <41401736+Nakheem@users.noreply.github.com>
2021-04-17 09:53:29 -0700

    index

--- php/header2.php
+++ php/header2.php
@@ -31,7 +31,7 @@ echo '<header>
       <ul class="nav navbar-nav">
         <li class="active"><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
-        <li><a href="#">Featured</a></li>
+        <li><a href="index.php">Featured</a></li>
         <li><a href="newPost.php">New Post</a></li>
         <li><a href="profile.php">My Account</a></li>
         <li><a href="contact.php">Contact Us</a></li>

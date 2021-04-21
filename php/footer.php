60e8d72d73ffad21c321919af1f4b83aa59ba387
Megan Nguyen <megan.nguyenn261@gmail.com>
2021-04-20 18:34:55 -0700

    Update footer styling

--- php/footer.php
+++ php/footer.php
@@ -4,12 +4,8 @@
 echo
 '<footer>
 	<div id="footer" class="footer-bottom row">
-	  <div class="col-md-6">
-		<ul class="nobullets">
-		  <li><a href="../php/index.php">Home</a></li>
-		  <li><a href="../php/about.php">About Us</a></li>
-		  <li><a href="../php/contact.php">Contacts</a></li>
-		</ul>
+	  <div class="col-md-12" style="padding-top:15px; padding-bottom:20px;">
+		  <p style="text-align: center;"><a href="../php/index.php">Home</a>	|	<a href="../php/about.php">About Us</a>	|	<a href="../php/contact.php">Contacts</a></p>
 	  </div>
 	</div>
 </footer>';

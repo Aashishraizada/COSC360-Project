9980128ec47fba9239a5840914712fddfae11b7b
Megan Nguyen <megan.nguyenn261@gmail.com>
2021-04-20 23:38:03 -0700

    footer to new post

--- php/contact.php
+++ php/contact.php
@@ -4,7 +4,7 @@
 <?php include "header1.php"; ?>
 
 <body>
-<?php include "header2.php"; ?>
+  <?php include "header2.php"; ?>
   <div id="main" class="section">
 
     <div class="container">
@@ -13,35 +13,35 @@
         <div id="main-post" class="col-md-8 hot-post-left">
           <div id="posts" class="post post-thumb col-md-12">
             <div class="col-md-12">
-            <h1>Contact Us</h1>
+              <h1>Contact Us</h1>
               <hr class="rounded post-divider">
             </div>
             <div class="post-body col-md-12">
-                <form method="post" action="processContact.php" id="contactUs" >
-                        <div class="col-md-2 hot-post-left">
-						First Name: </div>
-                        <input type="text" name="fname" id="fname" class="required">
-                        <br><br>
-						<div class="col-md-2 hot-post-left">
-                        Last Name: </div>
-                        <input type="text" name="lname" id="lname" class="required">
-                        <br><br>
-						<div class="col-md-2 hot-post-left">
-						Email: </div>
-                        <input type="text" name="email" id="email" class="required">
-                        <br><br>
-						<div class="col-md-2 hot-post-left">
-						Message: </div>
-                        <textarea type="text" name="message" id="message" class="required" rows="4" cols="50">
+              <form method="post" action="processContact.php" id="contactUs">
+                <div class="col-md-2 hot-post-left">
+                  First Name: </div>
+                <input type="text" name="fname" id="fname" class="required">
+                <br><br>
+                <div class="col-md-2 hot-post-left">
+                  Last Name: </div>
+                <input type="text" name="lname" id="lname" class="required">
+                <br><br>
+                <div class="col-md-2 hot-post-left">
+                  Email: </div>
+                <input type="text" name="email" id="email" class="required">
+                <br><br>
+                <div class="col-md-2 hot-post-left">
+                  Message: </div>
+                <textarea type="text" name="message" id="message" class="required" rows="4" cols="50">
                         </textarea>
-						<br><br>
-						<center><input type="submit" value="Send message" style="border:none"></center>
-                </form>
+                <br><br>
+                <center><input type="submit" value="Send message" style="border:none"></center>
+              </form>
             </div>
           </div>
         </div>
 
-		
+
 
       </div>
 
@@ -51,4 +51,5 @@
 
   <?php include "footer.php"; ?>
 </body>
+
 </html>
--- php/newPost.php
+++ php/newPost.php
@@ -107,18 +107,7 @@ if (!isset($_SESSION['username'])) {
         </div>
 
     </div>
-
-    <footer>
-        <div id="footer" class="footer-bottom row">
-            <div class="col-md-6">
-                <ul class="footer-nav">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About Us</a></li>
-                    <li><a href="contact.html">Contacts</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+	<?php include "footer.php"; ?>
 </body>
 
 </html>

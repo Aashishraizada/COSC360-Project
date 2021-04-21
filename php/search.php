7425ad4479a049139bafb02652dac48f0cf1bd10
Aashish Raizada <60690063+Aashishraizada@users.noreply.github.com>
2021-04-20 18:40:00 -0700

    Update search.php

--- php/search.php
+++ php/search.php
@@ -25,129 +25,30 @@ if (!is_null($key) && !empty($key)) {
 	$database = "myblogsite";
 	$user = "cosc360";
 	$password = "adminpassword";
-
-    $connection = new mysqli($host, $user, $password, $database);
+  
+  $connection = new mysqli($host, $user, $password, $database);
+  $sql = "";
+  $key = MySQLi_real_escape_string($connection, $key);
 
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
-    } else {
+    } else if($num == 1 || $num == 2 || $num == 3 || $num == 4) {
         if ($num == 1) { //posts based on description
-            $key = MySQLi_real_escape_string($connection, $key);
             $sql = "SELECT * FROM Post WHERE content LIKE '%".$key."%'";
-
-            $results = mysqli_query($connection, $sql);
-            while ($row = mysqli_fetch_assoc($results)) {
-                echo '<div id="posts" class="post post-thumb col-md-12">
-              <div class="post-header">
-                <h3 class="col-md-8 post-postTitle postTitle-lg"><a href="blog-post.html">' . $row["postTitle"] . '</a></h3>
-                <ul class="col-md-4 post-meta">
-                <li><a href="author.php?'.$temp.'"'.$row['userId'].'">' .$row['userId']. '</a></li>
-                  <li>' . $row['postDate'] . '</li>
-                </ul>
-              </div>
-              <div class="col-md-12">
-                <hr class="rounded post-divider">
-              </div>
-              <div class="post-body col-md-12">
-                <div class="col-md-7">
-                  <a class="post-img" href="blog-post.html" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
-                </div>
-                <div class="col-md-5">
-                  <p>' . $row['content'] . '</p>
-                </div>
-              </div>
-              <div class="post-footer col-md-12">
-                <ul class="nav navbar-nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">Keyword 1</a></li>
-                  <li><a href="#">Keyword 2</a></li>
-                </ul>
-              </div>
-            </div>';
-            }
-			if(mysqli_num_rows($results)) {
-				$num = 6;
-			}
-        } else if ($num == 2) { //posts based on keywords
+        } 
+        else if ($num == 2) { //posts based on keywords
             $sql = "SELECT * FROM Post JOIN Tags ON Post.postId = Tags.pid WHERE tagName LIKE '%".$key."%'";
-
-            $results = mysqli_query($connection, $sql);
-            while ($row = mysqli_fetch_assoc($results)) {
-                echo '<div id="posts" class="post post-thumb col-md-12">
-              <div class="post-header">
-                <h3 class="col-md-8 post-postTitle postTitle-lg"><a href="blog-post.html">' . $row["postTitle"] . '</a></h3>
-                <ul class="col-md-4 post-meta">
-                <li><a href="author.php?'.$temp.'"'.$row['userId'].'">' .$row['userId']. '</a></li>
-                  <li>' . $row['postDate'] . '</li>
-                </ul>
-              </div>
-              <div class="col-md-12">
-                <hr class="rounded post-divider">
-              </div>
-              <div class="post-body col-md-12">
-                <div class="col-md-7">
-                  <a class="post-img" href="blog-post.html" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
-                </div>
-                <div class="col-md-5">
-                  <p>' . $row['content'] . '</p>
-                </div>
-              </div>
-              <div class="post-footer col-md-12">
-                <ul class="nav navbar-nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">Keyword 1</a></li>
-                  <li><a href="#">Keyword 2</a></li>
-                </ul>
-              </div>
-            </div>';
-            }
-			if(mysqli_num_rows($results)) {
-				$num = 6;
-			}
-        } else if ($num == 3) { //posts based on user id
+        }
+        else if ($num == 3) { //posts based on user id
             $sql = "SELECT * FROM Post WHERE userId LIKE '%".$key."%'";
-
-            $results = mysqli_query($connection, $sql);
-            while ($row = mysqli_fetch_assoc($results)) {
-                echo '<div id="posts" class="post post-thumb col-md-12">
-              <div class="post-header">
-                <h3 class="col-md-8 post-postTitle postTitle-lg"><a href="blog-post.html">' . $row["postTitle"] . '</a></h3>
-                <ul class="col-md-4 post-meta">
-                <li><a href="author.php?'.$temp.'"'.$row['userId'].'">' .$row['userId']. '</a></li>
-                  <li>' . $row['postDate'] . '</li>
-                </ul>
-              </div>
-              <div class="col-md-12">
-                <hr class="rounded post-divider">
-              </div>
-              <div class="post-body col-md-12">
-                <div class="col-md-7">
-                  <a class="post-img" href="blog-post.html" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
-                </div>
-                <div class="col-md-5">
-                  <p>' . $row['content'] . '</p>
-                </div>
-              </div>
-              <div class="post-footer col-md-12">
-                <ul class="nav navbar-nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">Keyword 1</a></li>
-                  <li><a href="#">Keyword 2</a></li>
-                </ul>
-              </div>
-            </div>';
-            }
-			if(mysqli_num_rows($results)) {
-				$num = 6;
-			}
         } else if($num == 4){//posts from all users
             $sql = "SELECT * FROM Post";
-
-            $results = mysqli_query($connection, $sql);
-            while ($row = mysqli_fetch_assoc($results)) {
-                echo '<div id="posts" class="post post-thumb col-md-12">
+        }
+        $results = mysqli_query($connection, $sql);
+        while ($row = mysqli_fetch_assoc($results)) {
+            echo '<div id="posts" class="post post-thumb col-md-12">
               <div class="post-header">
-                <h3 class="col-md-8 post-postTitle postTitle-lg"><a href="blog-post.html">' . $row["postTitle"] . '</a></h3>
+                <h3 class="col-md-8 post-postTitle postTitle-lg">' . $row["postTitle"] . '</h3>
                 <ul class="col-md-4 post-meta">
                 <li><a href="author.php?'.$temp.'"'.$row['userId'].'">' .$row['userId']. '</a></li>
                   <li>' . $row['postDate'] . '</li>
@@ -158,7 +59,7 @@ if (!is_null($key) && !empty($key)) {
               </div>
               <div class="post-body col-md-12">
                 <div class="col-md-7">
-                  <a class="post-img" href="blog-post.html" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
+                  <a class="post-img" href="'.$row['postImage'].'" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
                 </div>
                 <div class="col-md-5">
                   <p>' . $row['content'] . '</p>
@@ -172,78 +73,79 @@ if (!is_null($key) && !empty($key)) {
                 </ul>
               </div>
             </div>';
+          }
+          if(mysqli_num_rows($results)) {
+            $num = 6;
+          }
+        } else {        
+            if($num == 5){//2 posts
+                $sql = "SELECT * FROM Post LIMIT 2";
+                echo '<p>No matching posts found. Please try again.</p>';
+                $results = mysqli_query($connection, $sql);
+                while ($row = mysqli_fetch_assoc($results)) {
+                    echo '<div id="posts" class="post post-thumb col-md-12" style="visibility:hidden">
+                  <div class="post-header">
+                    <h3 class="col-md-8 post-postTitle postTitle-lg">' . $row["postTitle"] . '</h3>
+                    <ul class="col-md-4 post-meta">
+                      <li><a href="author.html">' . $row['userId'] . '</a></li>
+                      <li>' . $row['postDate'] . '</li>
+                    </ul>
+                  </div>
+                  <div class="col-md-12">
+                    <hr class="rounded post-divider">
+                  </div>
+                  <div class="post-body col-md-12">
+                    <div class="col-md-7">
+                      <a class="post-img" href="'.$row['postImage'].'" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
+                    </div>
+                    <div class="col-md-5">
+                      <p>' . $row['content'] . '</p>
+                    </div>
+                  </div>
+                  <div class="post-footer col-md-12">
+                    <ul class="nav navbar-nav">
+                      <li class="active"><a href="#">Home</a></li>
+                      <li><a href="#">Keyword 1</a></li>
+                      <li><a href="#">Keyword 2</a></li>
+                    </ul>
+                  </div>
+                </div>';
+                }
             }
-			if(mysqli_num_rows($results)) {
-				$num = 6;
-			}
-        }  else if($num == 5){//2 posts
-            $sql = "SELECT * FROM Post LIMIT 2";
-			echo '<p>No matching posts found. Please try again.</p>';
-            $results = mysqli_query($connection, $sql);
-            while ($row = mysqli_fetch_assoc($results)) {
-                echo '<div id="posts" class="post post-thumb col-md-12" style="visibility:hidden">
-              <div class="post-header">
-                <h3 class="col-md-8 post-postTitle postTitle-lg"><a href="blog-post.html">' . $row["postTitle"] . '</a></h3>
-                <ul class="col-md-4 post-meta">
-                  <li><a href="author.html">' . $row['userId'] . '</a></li>
-                  <li>' . $row['postDate'] . '</li>
-                </ul>
-              </div>
-              <div class="col-md-12">
-                <hr class="rounded post-divider">
-              </div>
-              <div class="post-body col-md-12">
-                <div class="col-md-7">
-                  <a class="post-img" href="blog-post.html" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
-                </div>
-                <div class="col-md-5">
-                  <p>' . $row['content'] . '</p>
-                </div>
-              </div>
-              <div class="post-footer col-md-12">
-                <ul class="nav navbar-nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">Keyword 1</a></li>
-                  <li><a href="#">Keyword 2</a></li>
-                </ul>
-              </div>
-            </div>';
+            if($num == 6){//1 posts
+                $sql = "SELECT * FROM Post LIMIT 1";
+                $results = mysqli_query($connection, $sql);
+                while ($row = mysqli_fetch_assoc($results)) {
+                    echo '<div id="posts" class="post post-thumb col-md-12" style="visibility:hidden">
+                  <div class="post-header">
+                    <h3 class="col-md-8 post-postTitle postTitle-lg">' . $row["postTitle"] . '</h3>
+                    <ul class="col-md-4 post-meta">
+                      <li><a href="author.html">' . $row['userId'] . '</a></li>
+                      <li>' . $row['postDate'] . '</li>
+                    </ul>
+                  </div>
+                  <div class="col-md-12">
+                    <hr class="rounded post-divider">
+                  </div>
+                  <div class="post-body col-md-12">
+                    <div class="col-md-7">
+                      <a class="post-img" href="'.$row['postImage'].'" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
+                    </div>
+                    <div class="col-md-5">
+                      <p>' . $row['content'] . '</p>
+                    </div>
+                  </div>
+                  <div class="post-footer col-md-12">
+                    <ul class="nav navbar-nav">
+                      <li class="active"><a href="#">Home</a></li>
+                      <li><a href="#">Keyword 1</a></li>
+                      <li><a href="#">Keyword 2</a></li>
+                    </ul>
+                  </div>
+                </div>';
+                }
             }
-        }
-		if($num == 6){//1 posts
-            $sql = "SELECT * FROM Post LIMIT 1";
-            $results = mysqli_query($connection, $sql);
-            while ($row = mysqli_fetch_assoc($results)) {
-                echo '<div id="posts" class="post post-thumb col-md-12" style="visibility:hidden">
-              <div class="post-header">
-                <h3 class="col-md-8 post-postTitle postTitle-lg"><a href="blog-post.html">' . $row["postTitle"] . '</a></h3>
-                <ul class="col-md-4 post-meta">
-                  <li><a href="author.html">' . $row['userId'] . '</a></li>
-                  <li>' . $row['postDate'] . '</li>
-                </ul>
-              </div>
-              <div class="col-md-12">
-                <hr class="rounded post-divider">
-              </div>
-              <div class="post-body col-md-12">
-                <div class="col-md-7">
-                  <a class="post-img" href="blog-post.html" ><img src="'.$row['postImage'].'" alt="cat pic"></a>
-                </div>
-                <div class="col-md-5">
-                  <p>' . $row['content'] . '</p>
-                </div>
-              </div>
-              <div class="post-footer col-md-12">
-                <ul class="nav navbar-nav">
-                  <li class="active"><a href="#">Home</a></li>
-                  <li><a href="#">Keyword 1</a></li>
-                  <li><a href="#">Keyword 2</a></li>
-                </ul>
-              </div>
-            </div>';
-            }
-        }
+          }
         mysqli_free_result($results);
         mysqli_close($connection);
-    }
 }
